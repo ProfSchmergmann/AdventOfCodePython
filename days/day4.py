f0a7597ffc1eaeb1c5930d2f5534b69484047ecf
Sven Bergmann <sven.bergmann@rwth-aachen.de>
2022-12-06 14:56:16 +0100

    Finished day4

--- days/day4.py
+++ days/day4.py
@@ -4,11 +4,29 @@ def day4_1():
         for line in file.readlines():
             first_pair = line.split(',')[0]
             second_pair = line.split(',')[1].replace('\n', '')
-            first_pair_start = first_pair.split('-')[0]
-            first_pair_end = first_pair.split('-')[1]
-            second_pair_start = second_pair.split('-')[0]
-            second_pair_end = second_pair.split('-')[1]
+            first_pair_start = int(first_pair.split('-')[0])
+            first_pair_end = int(first_pair.split('-')[1])
+            second_pair_start = int(second_pair.split('-')[0])
+            second_pair_end = int(second_pair.split('-')[1])
             if ((second_pair_start <= first_pair_start <= first_pair_end <= second_pair_end) or
                     (first_pair_start <= second_pair_start <= second_pair_end <= first_pair_end)):
+                res += 1
+        return res
+
+
+def day4_2():
+    with open('res/day4_input.txt', 'r') as file:
+        res = 0
+        for line in file.readlines():
+            first_pair = line.split(',')[0]
+            second_pair = line.split(',')[1].replace('\n', '')
+            first_pair_start = int(first_pair.split('-')[0])
+            first_pair_end = int(first_pair.split('-')[1])
+            second_pair_start = int(second_pair.split('-')[0])
+            second_pair_end = int(second_pair.split('-')[1])
+            if ((second_pair_start <= first_pair_end <= second_pair_end) or
+                    (first_pair_start <= second_pair_end <= first_pair_end) or
+                    (second_pair_start <= first_pair_start <= second_pair_end) or
+                    (first_pair_start <= second_pair_start <= first_pair_end)):
                 res += 1
         return res
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from days.day4 import day4_1
+from days.day4 import day4_1, day4_2
 from days.day5 import day5_1, day5_2
 from days.day6 import day6_1, day6_2
 
 if __name__ == '__main__':
-    print(day5_2())
+    print(day4_2())
 
