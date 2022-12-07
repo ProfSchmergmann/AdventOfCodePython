268a1e24c35ba980b9bbd85e7ba3588804ccb24c
Sven Bergmann <sven.bergmann@rwth-aachen.de>
2022-12-07 12:21:36 +0100

    Prepared for Day 8.

---
+++ days/day8.py
@@ -0,0 +1,12 @@
+from days.day import Day
+
+
+class Day8(Day):
+    def __init__(self, number: int):
+        super().__init__(number)
+
+    def part_a(self):
+        return ''
+
+    def part_b(self):
+        return ''
--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from days.day4 import Day4
 from days.day5 import Day5
 from days.day6 import Day6
 from days.day7 import Day7
+from days.day8 import Day8
 
 if __name__ == '__main__':
-    day = Day7()
+    day = Day8()
     day.print_part_a_and_b_with_time()
