e497d8bfe160bb91982b09966570991b75a31dfd
ProfSchmergmann <sbergmann1995@gmail.com>
2022-12-09 20:31:08 +0100

    Prepared for Day 10.

---
+++ days/day10.py
@@ -0,0 +1,13 @@
+from days.day import Day
+
+
+class Day10(Day):
+
+    def __init__(self):
+        super().__init__(10)
+
+    def part_a(self):
+        return ''
+
+    def part_b(self):
+        return ''
