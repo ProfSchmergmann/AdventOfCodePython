103c4516278dcde992bca591203ff4a9558ff396
ProfSchmergmann <sbergmann1995@gmail.com>
2022-12-09 20:27:05 +0100

    Refactored code for day 4

--- days/day4.py
+++ days/day4.py
@@ -1,16 +1,6 @@
 from days.day import Day
 
 
-def split_pair_from_line(line):
-    first_pair = line.split(',')[0]
-    second_pair = line.split(',')[1].replace('\n', '')
-    first_pair_start = int(first_pair.split('-')[0])
-    first_pair_end = int(first_pair.split('-')[1])
-    second_pair_start = int(second_pair.split('-')[0])
-    second_pair_end = int(second_pair.split('-')[1])
-    return first_pair_start, first_pair_end, second_pair_start, second_pair_end
-
-
 class Day4(Day):
     def __init__(self):
         super().__init__(4)
@@ -18,19 +8,15 @@ class Day4(Day):
     def part_a(self):
         res = 0
         for line in self.get_lines_as_list():
-            first_pair_start, first_pair_end, second_pair_start, second_pair_end = split_pair_from_line(line)
-            if ((second_pair_start <= first_pair_start <= first_pair_end <= second_pair_end) or
-                    (first_pair_start <= second_pair_start <= second_pair_end <= first_pair_end)):
+            a, b, x, y = map(int, line.replace(",", "-").split("-"))
+            if (x <= a <= b <= y) or (a <= x <= y <= b):
                 res += 1
         return res
 
     def part_b(self):
         res = 0
         for line in self.get_lines_as_list():
-            first_pair_start, first_pair_end, second_pair_start, second_pair_end = split_pair_from_line(line)
-            if ((second_pair_start <= first_pair_end <= second_pair_end) or
-                    (first_pair_start <= second_pair_end <= first_pair_end) or
-                    (second_pair_start <= first_pair_start <= second_pair_end) or
-                    (first_pair_start <= second_pair_start <= first_pair_end)):
+            a, b, x, y = map(int, line.replace(",", "-").split("-"))
+            if (x <= b <= y) or (a <= y <= b) or (x <= a <= y) or (a <= x <= b):
                 res += 1
         return res
